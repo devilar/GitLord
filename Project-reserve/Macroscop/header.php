630e2cf63d6dd7ec9ed5354d03b0025f15e42bb8
devilar <devilar.sd@gmail.com>
2019-03-29 16:44:55 +0300

    make new backup for Macroscop

---
+++ Project-reserve/Macroscop/header.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html>
+  <head>
+        <title>[[SEO_meta_title? &docid=`[[*id]]`]]</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width">
+        <meta name="description" content="[[SEO_meta_descr? &docid=`[[*id]]`]]">
+        <meta name="keywords" content="[[*meta_key]]">
+        <link rel="shortcut icon" href="assets/favicon.ico">
+       
+        <base href="[[++site_url]][[*context_key:eq=`en`:then=`eocortex.com/`]]">
+        <meta property="og:title" content="[[*pagetitle]]">
+        <meta property="og:type" content="website">
+        <meta property="og:url" content="[[~[[*id]]]]">
+        <meta property="og:image" content="[[++site_url]]assets/img/logo.png">
+        <meta property="og:site_name" content="[[++site_name]]">
+        <meta property="og:description" content="[[*description]]">
+        <meta name="twitter:card" content="summary">
+        <meta name="twitter:site" content="[[++site_name]]">
+        <meta name="twitter:title" content="[[*pagetitle]]">
+        <meta name="twitter:description" content="[[*description]]">
+        <meta name="twitter:creator" content="[[++site_url]]">
+        <meta name="twitter:image:src" content="[[++site_url]]assets/img/logo.png">
+        <meta name="twitter:domain" content="[[~[[*id]]]]">
+        <link href="https://fonts.googleapis.com/css?family=PT+Sans:400,700%7CRoboto:400,700&amp;subset=latin,cyrillic" rel='stylesheet' type='text/css'>
+        <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+            <script src="/assets/js/html5shiv.min.js"></script>
+            <script src="/assets/js/html5shiv-printshiv.min.js"></script>
+            <script src="/assets/js/respond.min.js"></script>
+        <![endif]-->
+       
+        [[+css]]
+        <link href="/assets/css/owl.carousel.css" rel="stylesheet">
+       
+        <link href="/assets/css/style.css" rel="stylesheet">
+       <link rel="stylesheet" href="/assets/css/custom.css">
+       
+        <script type="text/javascript" src="/assets/components/ajaxform/js/lib/jquery.min.js"></script>
+        <script type="text/javascript" src="/assets/js/bootstrap.min.js"></script>
+        <script type="text/javascript" src="/assets/js/f.js"></script>
+        <script type="text/javascript" src="/assets/js/d-goals.js"></script>
+        <script type="text/javascript" src="/assets/jquery-ui-1.10.3.custom/js/jquery-ui-1.10.3.custom.min.js"></script>
+        [[+script]]
+ 
+<!-- End Facebook Pixel Code -->
+ 
+ 
+ 
+<script type="text/javascript">(window.Image ? (new Image()) : document.createElement('img')).src = location.protocol + '//vk.com/rtrg?r=dJDqUsz00q02zdE*kW/lmlw9rv8bfK3sGPGEIWCKJVx8sYvW0daRDh6vRHd2H6s0mH/eU0nm2QlHs3rlgfRDcuwfYkboX5uCf/dawXoWj3R0VbbWDc/DdAjKds3RwGmDaUY1EU6dtPLMpC*2uKE4G26ATExl6*uVEMxzFywF8is-&pixel_id=1000040240';</script>
+[[d-robots]]
+ 
+</head>
+    <body>
+        <!--[if lte IE 8]><script src="/assets/oldies/oldies.js" charset="utf-8"></script><![endif]-->
+        <nav id="inside-header" class="navbar navbar-default" role="navigation">
+            <div class="pre-header">
+                <div class="container text-right">
+                    [[pdoResources? &includeTVs=`openning_hours,head_phone` &depth=`1` &parents=`0` &where=`{"template:=":7}` &tpl=`head.contacts-v2` &context=`[[*context_key]]`]]
+                    <div class="clearfix hidden-sm hidden-md hidden-lg"></div>
+                    <div class="search">
+                        <form class="form-inline" action="[[~[[BabelTranslation? &resourceId=`329` &contextKey=`[[*context_key]]`]]]]">
+                            <div class="form-group">
+                                <label class="sr-only" for="query">[[%search? &topic=`macroscop` &namespace=`core` &language=`[[++cultureKey]]`]]</label>
+                                <input type="text" class="form-control" id="query" name="query" required="" placeholder="[[%search? &topic=`macroscop` &namespace=`core` &language=`[[++cultureKey]]`]]">
+                            </div>
+                            <button type="submit" class="btn btn-default"><span class="glyphicon glyphicon-search"> </span></button>
+                        </form>
+                    </div>
+                    [[!LoggedIn? &RID=`[[+RID]]`]]
+                    <div class="eng">
+                        <ul class="list-inline">
+                            [[BabelLinks]]
+                            [[*context_key:eq=`en`:then=`
+                            <li><a href= "http://macroscop.net/" target="_blank">Tr</li>
+                            `]]
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="container">
+                <div class="navbar-header logo">
+                    <a class="navbar-brand" href="[[++site_url]]"><img  src="/assets/img/logo.png" alt="[[++site_name]]"></a>
+                </div>
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#site-menu">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="site-menu">
+                    [[Wayfinder? &startId=`0` &level=`2` &fullLink=`0` &outerClass=`nav navbar-nav navbar-right` &firstClass=`0` &lastClass=`0` &excludeDocs=`3,4,3443,6,7,3444,3446,3493,8,9,3457,3466,3475,3484,10,63,66,221`]]
+                </div><!-- /.navbar-collapse -->
+            </div>
+            [[navigat]]
+           
+        </nav>
---
+++ Project-reserve/Macroscop/submenu.php
@@ -0,0 +1,80 @@
+<div class="breadcrumb-wrapper tar">
+    <div class="container">
+        <ol class="breadcrumb">
+            <li class="main"><a href="javascript:toggleMenu(true);">[[%all_site? &topic=`macroscop` &namespace=`core` &language=`[[++cultureKey]]`]]</a></li>
+            [[Wayfinder? &startId=`[[+id]]` &level=`1` &fullLink=`1` &firstClass=`0` &lastClass=`0` &outerTpl=`outerTpl` &excludeDocs=`4,3443,6,7,3444,3446,3493,8,9,3457,3466,3475,3484,10,63,66,221`]]
+        </ol>
+        <div class="allmenu" style="display: none;">
+            <div class="submenu">
+                <span class="pull-left"><a href="javascript:toggleMenu(false);">[[%all_site? &topic=`macroscop` &namespace=`core` &language=`[[++cultureKey]]`]]</a></span>
+                <a class="pull-right" href="javascript:toggleMenu(false);">[[%close? &topic=`macroscop` &namespace=`core` &language=`[[++cultureKey]]`]] &times;</a>
+            </div>
+            <div class="menu">
+                <div class="row">
+                
+                <div class="col-xs-6 col-sm-2">
+                    <ul>
+                        <li class="haschilds">
+                            <a href="[[~[[BabelTranslation? &resourceId=`37` &contextKey=`[[*context_key]]`]]]]">[[pdoField? &id=`[[BabelTranslation? &resourceId=`37` &contextKey=`[[*context_key]]`]]` &field=`pagetitle`]]</a>
+                        </li>
+                        <li style="margin-top:20px;">
+                            <a style="display:block; margin-bottom:-2px;" href="[[~[[BabelTranslation? &resourceId=`199` &contextKey=`[[*context_key]]`]]]]">[[pdoField? &id=`[[BabelTranslation? &resourceId=`199` &contextKey=`[[*context_key]]`]]` &field=`pagetitle`]]</a>
+                            [[pdoMenu? &level=`1` &resources=`-2370,-2336,-2357,-2359,-2360,-2361,-2363,-2364,-2365,-2366` &parents=`[[BabelTranslation? &resourceId=`37` &contextKey=`[[*context_key]]`]]` &showHidden=`1`]]
+                        </li>
+                    </ul>
+                </div>
+                <div class="col-xs-6 col-sm-2">
+                    <ul>
+                        <li class="haschilds">
+                            <a href="[[~[[BabelTranslation? &resourceId=`36` &contextKey=`[[*context_key]]`]]]]">[[pdoField? &id=`[[BabelTranslation? &resourceId=`36` &contextKey=`[[*context_key]]`]]` &field=`pagetitle`]]</a>
+                        </li>
+                        <li style="margin-top:20px;">
+                        </li>
+                    </ul>
+                </div>
+                <div class="col-xs-6 col-sm-2">
+                    <ul>
+                        <li class="haschilds">
+                            <a href="[[~[[BabelTranslation? &resourceId=`34` &contextKey=`[[*context_key]]`]]]]">[[pdoField? &id=`[[BabelTranslation? &resourceId=`34` &contextKey=`[[*context_key]]`]]` &field=`pagetitle`]]</a>
+                        </li>
+                        <li style="margin-top:20px;">
+                            [[pdoMenu? &level=`1` &parents=`[[BabelTranslation? &resourceId=`34` &contextKey=`[[*context_key]]`]]` &showHidden=`1`]]
+                        </li>
+                    </ul>
+                </div>
+                <div class="col-xs-6 col-sm-2">
+                    <ul>
+                        <li class="haschilds">
+                            <a href="[[~[[BabelTranslation? &resourceId=`35` &contextKey=`[[*context_key]]`]]]]">[[pdoField? &id=`[[BabelTranslation? &resourceId=`35` &contextKey=`[[*context_key]]`]]` &field=`pagetitle`]]</a>
+                        </li>
+                        <li style="margin-top:20px;">
+                            [[pdoMenu? &level=`1` &parents=`[[BabelTranslation? &resourceId=`35` &contextKey=`[[*context_key]]`]]` &showHidden=`1`]]
+                        </li>
+                    </ul>
+                </div>
+                <div class="col-xs-6 col-sm-2">
+                    <ul>
+                        <li class="haschilds">
+                            <a href="[[~[[BabelTranslation? &resourceId=`43` &contextKey=`[[*context_key]]`]]]]">[[pdoField? &id=`[[BabelTranslation? &resourceId=`43` &contextKey=`[[*context_key]]`]]` &field=`pagetitle`]]</a>
+                        </li>
+                        <li style="margin-top:20px;">
+                            [[pdoMenu? &level=`1` &parents=`[[BabelTranslation? &resourceId=`43` &contextKey=`[[*context_key]]`]]` &showHidden=`1`]]
+                        </li>
+                    </ul>
+                </div>
+                
+                <div class="col-xs-6 col-sm-2">
+                    <ul>
+                        <li class="haschilds">
+                            <a href="[[~[[BabelTranslation? &resourceId=`5` &contextKey=`[[*context_key]]`]]]]">[[pdoField? &id=`[[BabelTranslation? &resourceId=`5` &contextKey=`[[*context_key]]`]]` &field=`pagetitle`]]</a>
+                        </li>
+                        <li style="margin-top:20px;">
+                            [[pdoMenu? &level=`1` &parents=`[[BabelTranslation? &resourceId=`5` &contextKey=`[[*context_key]]`]]` &showHidden=`1` &resources=`-4,-3443,-6,-7,-3444,-3446,-3493,-8,-9,-3457,-3466,-3475,-3484,-10,-63,-66,-221`]]
+                        </li>
+                    </ul>
+                </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
